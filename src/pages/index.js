e0d730f4bce0c1e3cfe2c8c3f544c15b9e11005f
Felipe Kaled <fekaled@gmail.com>
2021-09-03 17:04:43 -0300

    Finished Gatsby Tutorial part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return(
     <Layout pageTitle="Home Page">
       <p>Queremos dividir nossa experiência para ajudar a construir o seu projeto</p>
+      <StaticImage
+      alt="Ëdificio Boa Sorte real"
+      src="../images/ed-boa-sorte.png"
+      />
     </Layout>
   )
 }
