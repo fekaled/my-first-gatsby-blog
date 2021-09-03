bb125a0ff64a1797e3deead2105a2aeb4c5122c3
Anita <anita@MacBook-Pro-13.local>
2021-09-03 00:34:00 -0300

    Finished Gatsby tutotial part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="Apresentação">
+      <p>Aqui vamos descrever como chegamos nessa ideia para juntarmos nossas experiências para formar o edificio boa sorte</p>
+    </Layout>
+    )
+}
+
+export default AboutPage
